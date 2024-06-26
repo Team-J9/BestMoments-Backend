c74cc7582876cdf0177e608c5ccfb742fa2d3c8b
SJ70 <seojae70@gmail.com>
2024-06-26 23:58:43 +0900

    chore: init spring boot

---
+++ src/main/java/com/j9/bestmoments/BestMomentsApplication.java
@@ -0,0 +1,13 @@
+package com.j9.bestmoments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestMomentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestMomentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/j9/bestmoments/BestMomentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j9.bestmoments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestMomentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
