34152ec517e655ae4f4651085029a8542de8b30d
SJ70 <seojae70@gmail.com>
2024-06-27 16:54:58 +0900

    chore: security 기본 설정

---
+++ src/main/java/com/j9/bestmoments/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.j9.bestmoments.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+
+                .authorizeHttpRequests(auth ->
+                        auth.anyRequest().permitAll());
+
+        return http.build();
+    }
+
+}
