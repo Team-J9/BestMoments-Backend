c1a07bde0add9d37e73fc29f898c3e3790a9761e
SJ70 <seojae70@gmail.com>
2024-07-07 23:07:28 +0900

    fix: 토큰과 사용자의 연관관계 수정
    - 한 번에 여러 리프래시 토큰을 가질 수 있음

--- src/main/java/com/j9/bestmoments/domain/Token.java
+++ src/main/java/com/j9/bestmoments/domain/Token.java
@@ -5,7 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.ManyToOne;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,7 +19,7 @@ public class Token {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "member_id", referencedColumnName = "id")
     private Member member;
 
