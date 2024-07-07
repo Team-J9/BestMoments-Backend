4762391de2b1647ded7cd35f3ed45c06565e17a5
SJ70 <seojae70@gmail.com>
2024-07-08 02:23:22 +0900

    feat: 사용자 상세 정보 조회
    - 사용자 id로 조회

--- src/main/java/com/j9/bestmoments/controller/MemberController.java
+++ src/main/java/com/j9/bestmoments/controller/MemberController.java
@@ -1,6 +1,7 @@
 package com.j9.bestmoments.controller;
 
 import com.j9.bestmoments.domain.Member;
+import com.j9.bestmoments.dto.response.MemberFindDto;
 import com.j9.bestmoments.dto.response.MemberSummaryDto;
 import com.j9.bestmoments.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -10,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +29,13 @@ public class MemberController {
         UUID memberId = UUID.fromString(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
         Member member = memberService.findById(memberId);
         return ResponseEntity.ok(MemberSummaryDto.of(member));
+    }
+
+    @GetMapping("/{memberId}")
+    @Operation(summary = "사용자 상세 정보 조회", description = "사용자 id로 상세 정보 조회")
+    public ResponseEntity<MemberFindDto> getMemberInfo(@PathVariable String memberId) {
+        Member member = memberService.findById(memberId);
+        return ResponseEntity.ok(MemberFindDto.of(member));
     }
 
 }
---
+++ src/main/java/com/j9/bestmoments/dto/response/MemberFindDto.java
@@ -0,0 +1,21 @@
+package com.j9.bestmoments.dto.response;
+
+import com.j9.bestmoments.domain.Member;
+
+public record MemberFindDto(
+        String id,
+        String name,
+        String email,
+        String profileImageUrl
+) {
+
+    public static MemberFindDto of(Member member) {
+        return new MemberFindDto(
+                member.getId().toString(),
+                member.getName(),
+                member.getEmail(),
+                member.getProfileImageUrl()
+        );
+    }
+
+}
