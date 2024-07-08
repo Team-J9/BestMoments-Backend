980bce483a5f89f983b93f7e2a10df9d2e4fcac7
SJ70 <seojae70@gmail.com>
2024-07-08 20:08:21 +0900

    feat: 관리자의 사용자 조회 api

--- src/main/java/com/j9/bestmoments/controller/AdminMemberController.java
+++ src/main/java/com/j9/bestmoments/controller/AdminMemberController.java
@@ -5,11 +5,13 @@ import com.j9.bestmoments.dto.response.MemberFindDto;
 import com.j9.bestmoments.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +29,12 @@ public class AdminMemberController {
         Page<Member> members = memberService.findAll(pageable);
         return ResponseEntity.ok(members.map(MemberFindDto::of));
     }
-    
+
+    @Operation(summary = "사용자 상세 정보 조회")
+    @GetMapping("/{memberId}")
+    public ResponseEntity<MemberFindDto> findById(@PathVariable UUID memberId) {
+        Member member = memberService.findById(memberId);
+        return ResponseEntity.ok(MemberFindDto.of(member));
+    }
+
 }
