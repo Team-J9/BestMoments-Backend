b12f84215f0ac449505a7db6f4a45aa25b3c520c
SJ70 <seojae70@gmail.com>
2024-07-19 18:01:33 +0900

    feat: HttpMessageNotReadableException 핸들러 작성

--- src/main/java/com/j9/bestmoments/exception/GlobalExceptionHandler.java
+++ src/main/java/com/j9/bestmoments/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -25,6 +26,12 @@ public class GlobalExceptionHandler {
                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .collect(Collectors.joining(", "));
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(msg);
+    }
+
+    // 잘못된 형식, 매핑 실패, 필드 누락
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    protected ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
     }
 
 }
