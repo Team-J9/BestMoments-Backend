b2c8b79adfa4645fe3edc6d810b6029d702a851a
SJ70 <seojae70@gmail.com>
2024-07-19 17:49:02 +0900

    feat: 사용자 이름, 자기소개 문자열 길이 검증

--- src/main/java/com/j9/bestmoments/dto/request/MemberUpdateDto.java
+++ src/main/java/com/j9/bestmoments/dto/request/MemberUpdateDto.java
@@ -1,8 +1,21 @@
 package com.j9.bestmoments.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public record MemberUpdateDto (
+
+        @NotNull
+        @NotBlank
+        @Size(max = 20, message = "이름은 20자 이내로 입력해주세요.")
         String name,
+
+        @NotNull
+        @NotBlank
+        @Size(max = 1000, message = "자기소개는 1000자 이내로 입력해주세요.")
         String description
+
 ) {
 
 }
