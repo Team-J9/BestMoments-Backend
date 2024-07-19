d354a111a27189822f45aee468eba1634629d2f8
SJ70 <seojae70@gmail.com>
2024-07-19 15:58:34 +0900

    feat: EntityNotFoundException 핸들러 작성

---
+++ src/main/java/com/j9/bestmoments/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.j9.bestmoments.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    protected ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+}
