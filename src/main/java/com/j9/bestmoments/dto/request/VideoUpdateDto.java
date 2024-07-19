f9799e2a79a720657c2b10acd85856af53e2aae1
SJ70 <seojae70@gmail.com>
2024-07-19 17:24:20 +0900

    feat: 동영상 제목, 상세설명 길이 제한 및 예외 처리
    - 생성 DTO, 수정 DTO에 적용

--- src/main/java/com/j9/bestmoments/dto/request/VideoUpdateDto.java
+++ src/main/java/com/j9/bestmoments/dto/request/VideoUpdateDto.java
@@ -1,11 +1,22 @@
 package com.j9.bestmoments.dto.request;
 
 import com.j9.bestmoments.domain.VideoStatus;
-import org.springframework.web.multipart.MultipartFile;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record VideoUpdateDto(
+
+        @NotNull
+        @NotBlank
+        @Size(max = 100, message = "제목은 100자 이내로 입력해주세요.")
         String title,
+
+        @NotNull
+        @NotBlank
+        @Size(max = 2000, message = "상세설명은 2000자 이내로 입력해주세요.")
         String description,
+
         VideoStatus videoStatus
 ) {
 
--- src/main/java/com/j9/bestmoments/exception/GlobalExceptionHandler.java
+++ src/main/java/com/j9/bestmoments/exception/GlobalExceptionHandler.java
@@ -1,8 +1,11 @@
 package com.j9.bestmoments.exception;
 
 import jakarta.persistence.EntityNotFoundException;
+import java.util.stream.Collectors;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -12,6 +15,16 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(EntityNotFoundException.class)
     protected ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        String msg = e.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.joining(", "));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(msg);
     }
 
 }
