a9b9506d3d6fc5d4fb86b0329df8009e1cb1f786
SJ70 <seojae70@gmail.com>
2024-09-25 13:59:44 +0900

    test: 수정된 생성자에 대해 변경되지 않았던 테스트 코드 수정

--- src/test/java/com/j9/bestmoments/util/VideoGenerator.java
+++ src/test/java/com/j9/bestmoments/util/VideoGenerator.java
@@ -7,13 +7,14 @@ import com.j9.bestmoments.domain.VideoStatus;
 public final class VideoGenerator {
 
     public static Video createVideo(Member member, String title, VideoStatus videoStatus) {
-        return Video.builder()
+        Video video = Video.builder()
                 .uploader(member)
-                .fileUrl(title + "'s fileUrl")
                 .title(title)
                 .description(title + "'s description")
                 .videoStatus(videoStatus)
                 .build();
+        video.setVideoUrl(title + "'s fileUrl");
+        return video;
     }
 
 }
